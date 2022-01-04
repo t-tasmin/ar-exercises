55221d074f8909a6a9d00838cf15652564b1c70b
Tania Tasmin <taniat.kaau@email.com>
2022-01-04 13:01:08 -0500

    Complete Exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# stores = Store.all
+# puts stores
+# puts stores.count
+
+@store1 = Store.first   
+@store2 = Store.second
+
+@store1.name = 'Oshawa'
+puts(@store1.name)
