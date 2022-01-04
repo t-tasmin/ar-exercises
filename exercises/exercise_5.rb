e2de2603c3ad0653b9d9f2de67e0e46bc716ca33
Tania Tasmin <taniat.kaau@email.com>
2022-01-04 13:35:24 -0500

    Complete Exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@sum= Store.sum(:annual_revenue)
+@average=Store.average(:annual_revenue)
+
+puts @sum
+puts @average
+
+@no_of_stores=Store.where("annual_revenue > 1000000").count
+puts @no_of_stores
